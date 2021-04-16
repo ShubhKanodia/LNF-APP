9be071c7fd57138c3af2d6b3df81190ce014144c
Dhruv Shetty <dhruvshetty3@gmail.com>
2021-04-16 12:59:25 +0530

    Refactoring code

--- lib/themeintro.dart
+++ lib/themeintro.dart
@@ -1,37 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'assets.dart';
 import 'package:google_fonts/google_fonts.dart';
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'LearnNFun',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+import 'assets.dart';
 
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+class ThemeIntro extends StatefulWidget {
+  ThemeIntro({Key key}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -43,13 +16,10 @@ class MyHomePage extends StatefulWidget {
   // always marked "final".
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ThemeIntroState createState() => _ThemeIntroState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
-
-
+class _ThemeIntroState extends State<ThemeIntro> {
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
@@ -64,125 +34,126 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Color(0xffc1c1c1),
       body: Center(
 
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(height: height*0.15,),
-              Container(
-                width: width*0.84 ,
-                height: height * 0.7,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(height*0.1),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0x338b8b8b),
-                      blurRadius: 4,
-                      offset: Offset(0, 4),
-
-                    ),
-                  ],
-                  color: Colors.white,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            height: height * 0.15,
+          ),
+          Container(
+            width: width * 0.84,
+            height: height * 0.7,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(height * 0.1),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0x338b8b8b),
+                  blurRadius: 4,
+                  offset: Offset(0, 4),
                 ),
-                child: Column(/// TODO: Replace lock image with gift and a cross
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              ],
+              color: Colors.white,
+            ),
+            child: Column(
+              /// TODO: Replace lock image with gift and a cross
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  height: height * 0.1,
+                ),
+                Container(
+                  height: height * 0.08,
+                  width: width * 0.45,
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          left: width * 0.05, right: width * 0.05),
+                      child: Assets.lockImage,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: height * 0.1,
+                ),
+                Column(
+                  /// TODO: Left and right padding on the text
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(height: height*0.1,),
-                    Container(
-                      height:height*0.08,
-                      width: width*0.45,
-                      child: FittedBox(
-                        fit: BoxFit.cover,
-                        child: Padding(
-                          padding: EdgeInsets.only(left:width*0.05, right:width*0.05),
-                          child: Assets.lockImage,
-                        ),
+                    Text(
+                      "Gift a Friend",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.quicksand(
+                          color: Color(0xff489fb5),
+                          fontSize: height * 0.04,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Design a custom gift by using the Design Thinking method!",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.quicksand(
+                        color: Color(0xff489fb5),
+                        fontSize: height * 0.02,
                       ),
                     ),
-                    Container(height: height*0.1,),
-
-                    Column(/// TODO: Left and right padding on the text
+                    Text(
+                      "Learn how to understand a user, and solve their problems creatively. As you progress, unlock more challenging levels and complete your course! Have fun!",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.quicksand(
+                        color: Color(0xff489fb5),
+                        fontSize: height * 0.02,
+                      ),
+                    ),
+                  ],
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    margin: EdgeInsets.only(top: height * 0.01),
+                    height: height * 0.09,
+                    width: width * 0.65,
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0xffca7602),
+                            blurRadius: 0.0, // soften the shadow
+                            spreadRadius: 2.0, //extend the shadow
+                            offset: Offset(
+                              0.0, // Move to right 10  horizontally
+                              4.0, // Move to bottom 10 Vertically
+                            ),
+                          )
+                        ],
+                        color: Color(0xffffa62b),
+                        border: Border.all(
+                          color: Color(0xffffa62b),
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(30))),
+                    child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         Text(
-                          "Gift a Friend",
-                          textAlign: TextAlign.center,
+                          "Continue",
                           style: GoogleFonts.quicksand(
-                            color: Color(0xff489fb5),
-                            fontSize: height*0.04,
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                        Text(
-                          "Design a custom gift by using the Design Thinking method!",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.quicksand(
-                              color: Color(0xff489fb5),
-                              fontSize: height*0.02,
-                          ),
-                        ),
-                        Text(
-                          "Learn how to understand a user, and solve their problems creatively. As you progress, unlock more challenging levels and complete your course! Have fun!",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.quicksand(
-                              color: Color(0xff489fb5),
-                              fontSize: height*0.02,
-                          ),
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: height * 0.04),
                         ),
                       ],
                     ),
-                    GestureDetector(
-                      onTap: (){
-
-                      },
-                      child: Container(
-                        margin: EdgeInsets.only(top: height*0.01),
-                        height: height*0.09,
-                        width: width*0.65,
-                        decoration: BoxDecoration(
-                            boxShadow: [
-
-                              BoxShadow(
-                                color: Color(0xffca7602),
-                                blurRadius: 0.0, // soften the shadow
-                                spreadRadius: 2.0, //extend the shadow
-                                offset: Offset(
-                                  0.0, // Move to right 10  horizontally
-                                  4.0, // Move to bottom 10 Vertically
-                                ),
-                              )
-                            ],
-                            color: Color(0xffffa62b),
-                            border: Border.all(color: Color(0xffffa62b),
-
-                            ),
-                            borderRadius: BorderRadius.all(Radius.circular(30))
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text("Continue",style: GoogleFonts.quicksand(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: height*0.04
-                            ),),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(height: height*0.03,),
-
-                  ],
+                  ),
                 ),
-              ),
-
-
-            ],
-          )
-
-      ),
+                Container(
+                  height: height * 0.03,
+                ),
+              ],
+            ),
+          ),
+        ],
+      )),
 
       // This trailing comma makes auto-formatting nicer for build methods.
     );
