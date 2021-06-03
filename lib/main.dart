ca8e94012d39796b64bfbe8787b0f86c7369eb26
adarshnair <63840678+adarshnair427@users.noreply.github.com>
2021-06-03 20:55:21 +0530

    Library Tabs

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,7 @@ class MyApp extends StatelessWidget {
                         accentColor: const Color(0xffffa62b),
                       ),
                       home:
-                      Library())); //TODO: Change this to whatever you want while testing, but change to Levels() before pushing
+                      Levels())); //TODO: Change this to whatever you want while testing, but change to Levels() before pushing
 
 
               //TaskCompleted(rewards: 10, task: true, trophies: 10)));
