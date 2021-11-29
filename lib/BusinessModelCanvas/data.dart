7f40436375f3966573a8a7ccc89f93db27dab3cf
adarshnair <63840678+adarshnair427@users.noreply.github.com>
2021-11-29 18:26:03 +0530

    Checkboxes

--- lib/BusinessModelCanvas/data.dart
+++ lib/BusinessModelCanvas/data.dart
@@ -1,19 +1,35 @@
 List<String> siloTextOptions = ["Sell"];
 
+class BMCElement{
+  String name;
+  int score;
+  bool selected;
+  BMCElement({
+    this.name,this.score,this.selected
+});
 
-Map<String,Map<String,int>> farmScores = {
-  "Key Partners":{
-    "Retail Partnerships/Grocers": 5,
-    "Food Quality Certifiers":5,
-    "Packaging and Delivery Partners": 5,
-    "Online retailers": 5,
-    "Marketing Consultant": 0,
-    "Doctors/Health Specialists": 0,
-    "Consumers":-5,
-    "Labourers":-5,
-  },
-  "Value Proposition":{
-
-  }
+  Map toJson() => {
+    'name': name,
+    'score': score,
+    'selected':selected,
 };
+}
+
+Map<String,List<BMCElement>> farmScores = {
+  "Key Partners": [
+    BMCElement(name: "Retail Partnerships/Grocers", score: 5, selected: false),
+    BMCElement(name: "Food Quality Certifiers", score: 5, selected: false),
+    BMCElement(name: "Packaging and Delivery Partners", score: 5, selected: false),
+    BMCElement(name: "Online retailers", score: 5, selected: false),
+    BMCElement(name: "Marketing Consultant", score: 0, selected: false),
+    BMCElement(name: "Doctors/Health Specialists", score: 0, selected: false),
+    BMCElement(name: "Consumers", score: -5, selected: false),
+    BMCElement(name: "Labourers", score: -5, selected: false),
+  ],
+  "Value Proposition":[
+
+  ]
+};
+
+
 
