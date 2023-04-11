717a0ba502615ab038d00e64dacf4d6b893c3e12
Logic-4 <sharath.shashidhara@gmail.com>
2023-04-11 22:31:24 +0530

    first_setup

--- lib/DesignThinking/tasks/3_ideate.dart
+++ lib/DesignThinking/tasks/3_ideate.dart
@@ -18,50 +18,49 @@ class Ideate extends StatefulWidget {
 }
 
 class _IdeateState extends State<Ideate> {
-  get currentPersona => currentProgress.playingLevel2?currentPersonaL2:currentPersonaL1;
-  get score => currentProgress.playingLevel2?l2Score:l1Score;
-  int time=5;
+  get currentPersona =>
+      currentProgress.playingLevel2 ? currentPersonaL2 : currentPersonaL1;
+  get score => currentProgress.playingLevel2 ? l2Score : l1Score;
+  int time = 5;
   Timer timer;
   List<Color> colors = [];
 
-  void initState(){
+  void initState() {
     super.initState();
-    colors.addAll(List.generate(currentPersona.ideas.length, (index) =>  Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(0.2)));
+    colors.addAll(List.generate(
+        currentPersona.ideas.length,
+        (index) => Color((Random().nextDouble() * 0xFFFFFF).toInt())
+            .withOpacity(0.2)));
   }
 
-  void dispose(){
+  void dispose() {
     super.dispose();
     timer.cancel();
   }
 
-
-  void startTimer(){
-    timer = Timer.periodic(Duration(seconds: 1),
-    (Timer timer) {
-      if (time == 0 || currentPersona.ideas.length==0) {
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (Timer timer) {
+      if (time == 0 || currentPersona.ideas.length == 0) {
         timer.cancel();
-        if(currentProgress.playingLevel2){
+        if (currentProgress.playingLevel2) {
           userDocReference.update({
-
-            "rewards":FieldValue.increment(1),
-            "taskUnlocked":12,
-            "L2T3":l2Score.task[3],
+            "rewards": FieldValue.increment(1),
+            "taskUnlocked": 12,
+            "L2T3": l2Score.task[3],
           });
 
           currentProgress.taskUnlocked = 12;
-          currentProgress.rewards+=1;
-        }else{
+          currentProgress.rewards += 1;
+        } else {
           userDocReference.update({
-
-            "rewards":FieldValue.increment(1),
-            "taskUnlocked":4,
-            "L1T3":l1Score.task[3],
+            "rewards": FieldValue.increment(1),
+            "taskUnlocked": 4,
+            "L1T3": l1Score.task[3],
           });
 
           currentProgress.taskUnlocked = 4;
-          currentProgress.rewards+=1;
+          currentProgress.rewards += 1;
         }
-
 
         Navigator.push(
             context,
@@ -76,9 +75,9 @@ class _IdeateState extends State<Ideate> {
     });
   }
 
-/// Keep first one blank for some reason
+  /// Keep first one blank for some reason
 
-List storedIdeas = [];
+  List storedIdeas = [];
 
   @override
   Widget build(BuildContext context) {
@@ -108,52 +107,58 @@ List storedIdeas = [];
                   ],
                 )),
             Expanded(
-              child:BubbleChartLayout(
-                root: BubbleNode.node(
-                  padding:20,
-                  children: currentPersona.ideas.length==0?[]:List.generate(currentPersona.ideas.length, (index) => bubble(currentPersona.ideas.keys.elementAt(index), index, height) ),
-              ),
-              ),
+              child: BubbleChartLayout(children: [
+                BubbleNode.node(
+                  padding: 20,
+                  children: currentPersona.ideas.length == 0
+                      ? []
+                      : List.generate(
+                          currentPersona.ideas.length,
+                          (index) => bubble(
+                              currentPersona.ideas.keys.elementAt(index),
+                              index,
+                              height)),
+                ),
+              ]),
             ),
             Container(
-              width:width,
-                height: height * 0.1,
-                decoration: BoxDecoration(color: Color(0xfff4f4f4)),
-            child: Text("Pop the bubbles with the best ideas",
-                style: GoogleFonts.quicksand(
-                    color: const Color(0xffffa62b),
-                    fontWeight: FontWeight.w500,
-                    fontStyle: FontStyle.normal,
-                    fontSize: height * 0.028),
-                textAlign: TextAlign.left),),
+              width: width,
+              height: height * 0.1,
+              decoration: BoxDecoration(color: Color(0xfff4f4f4)),
+              child: Text("Pop the bubbles with the best ideas",
+                  style: GoogleFonts.quicksand(
+                      color: const Color(0xffffa62b),
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.normal,
+                      fontSize: height * 0.028),
+                  textAlign: TextAlign.left),
+            ),
           ],
         ))));
   }
 
-
   BubbleNode bubble(String text, int index, height) {
-    return BubbleNode.leaf(value: 1000,
-    options: BubbleOptions(
-        onTap: () {
-          if(timer==null || !timer.isActive) startTimer();
-          score.task[3]=0;
-          if(currentPersona.ideas[text] == true) score.task[3]++;
-          if(currentPersona.ideas[text] == false) score.task[3]--;
-          setState(() {
-            currentPersona.ideas.remove(text);
-          });
-          storedIdeas.add(text);
-        },
-      color:  colors[index],
-      child:  Text(text,
-                  style: GoogleFonts.quicksand(
-                      color: const Color(0xff489fb5),
-                      fontWeight: FontWeight.w700,
-                      fontStyle: FontStyle.normal,
-                      fontSize: height*0.02),
-                  textAlign: TextAlign.center))
-
-    );
+    return BubbleNode.leaf(
+        value: 1000,
+        options: BubbleOptions(
+            onTap: () {
+              if (timer == null || !timer.isActive) startTimer();
+              score.task[3] = 0;
+              if (currentPersona.ideas[text] == true) score.task[3]++;
+              if (currentPersona.ideas[text] == false) score.task[3]--;
+              setState(() {
+                currentPersona.ideas.remove(text);
+              });
+              storedIdeas.add(text);
+            },
+            color: colors[index],
+            child: Text(text,
+                style: GoogleFonts.quicksand(
+                    color: const Color(0xff489fb5),
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.normal,
+                    fontSize: height * 0.02),
+                textAlign: TextAlign.center)));
   }
 }
 
@@ -163,87 +168,87 @@ class Ideate2 extends StatefulWidget {
 }
 
 class _Ideate2State extends State<Ideate2> {
-
   Timer timer;
-  int time=20; //TODO : Change to 300
-  void startTimer(){
-    timer = Timer.periodic(Duration(seconds: 1),
-            (Timer timer) {
-          if (time == 0 && mounted) {
-            timer.cancel();
-            userDocReference.update({
+  int time = 20; //TODO : Change to 300
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (Timer timer) {
+      if (time == 0 && mounted) {
+        timer.cancel();
+        userDocReference
+            .update({"rewards": FieldValue.increment(1), "taskUnlocked": 4});
 
-              "rewards":FieldValue.increment(1),
-              "taskUnlocked":4
-            });
+        currentProgress.taskUnlocked = 4;
+        currentProgress.rewards += 1;
 
-            currentProgress.taskUnlocked = 4;
-            currentProgress.rewards+=1;
-
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => InformationCard(cardNumber: 2),
-                    settings: RouteSettings(name: 'Actual game')));
-          } else {
-            setState(() {
-              time--;
-            });
-          }
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => InformationCard(cardNumber: 2),
+                settings: RouteSettings(name: 'Actual game')));
+      } else {
+        setState(() {
+          time--;
         });
+      }
+    });
   }
 
   TextEditingController idea = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
     return Scaffold(
         backgroundColor: Color(0xfff4f4f4),
-    body: Center(
-      child: Column(
-        children: [
-          StdBackButton(),
-        Text("Ideate",
-          style: GoogleFonts.quicksand(
-              color: const Color(0xff489fb5),
-              fontWeight: FontWeight.w700,
-              fontStyle: FontStyle.normal,
-              fontSize: height * 0.05)
-        ),
-          Padding(
-            padding: EdgeInsets.only(left:width*0.2, right : width*0.2, top:height*0.001, bottom:height*0.001 ),
-            child: Text("List all the ideas you come up with for gifts in the next 5 minutes. The timer will begin once you tap on the page.",
+        body: Center(
+            child: Column(
+          children: [
+            StdBackButton(),
+            Text("Ideate",
                 style: GoogleFonts.quicksand(
-                    color: const Color(0xff1a1b41),
-                    fontWeight: FontWeight.w400,
+                    color: const Color(0xff489fb5),
+                    fontWeight: FontWeight.w700,
                     fontStyle: FontStyle.normal,
-                    fontSize: height * 0.02)
+                    fontSize: height * 0.05)),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: width * 0.2,
+                  right: width * 0.2,
+                  top: height * 0.001,
+                  bottom: height * 0.001),
+              child: Text(
+                  "List all the ideas you come up with for gifts in the next 5 minutes. The timer will begin once you tap on the page.",
+                  style: GoogleFonts.quicksand(
+                      color: const Color(0xff1a1b41),
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: height * 0.02)),
             ),
-          ),
-        Expanded(
-          child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: Svg('assets/ideation/notepad.svg'),
-              fit: BoxFit.fill
-
-            ),
-          ),
-              child:
-             TextField(
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: Svg('assets/ideation/notepad.svg'),
+                      fit: BoxFit.fill),
+                ),
+                child: TextField(
                   onTap: startTimer,
                   controller: idea,
                   keyboardType: TextInputType.multiline,
-                  minLines: 1,//Normal textInputField will be displayed
+                  minLines: 1, //Normal textInputField will be displayed
                   maxLines: 10,
                   style: TextStyle(
-                    height: MediaQuery.of(context).viewInsets.bottom==0?height*0.0028:height*0.0013,
+                    height: MediaQuery.of(context).viewInsets.bottom == 0
+                        ? height * 0.0028
+                        : height * 0.0013,
                   ),
                   decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: width*0.15, right: width*0.1, top: height*0.04, bottom: height*0.02 ),
+                    contentPadding: EdgeInsets.only(
+                        left: width * 0.15,
+                        right: width * 0.1,
+                        top: height * 0.04,
+                        bottom: height * 0.02),
                     hintText: "Type here...",
                     border: InputBorder.none,
                     focusedBorder: InputBorder.none,
@@ -251,24 +256,25 @@ class _Ideate2State extends State<Ideate2> {
                     errorBorder: InputBorder.none,
                     disabledBorder: InputBorder.none,
                   ),
-
                 ),
-          ),
-        ),
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text(time>60?time%60>10?"Time left- ${(time~/60)}:${(time%60)} minutes":"Time left- ${(time~/60)}:0${(time%60)} minutes":"Time left- $time seconds",
-              style: GoogleFonts.quicksand(
-                  color: const Color(0xffffa62b),
-                  fontWeight: FontWeight.w500,
-                  fontStyle: FontStyle.normal,
-                  fontSize: height * 0.04),
-              textAlign: TextAlign.left),
+              child: Text(
+                  time > 60
+                      ? time % 60 > 10
+                          ? "Time left- ${(time ~/ 60)}:${(time % 60)} minutes"
+                          : "Time left- ${(time ~/ 60)}:0${(time % 60)} minutes"
+                      : "Time left- $time seconds",
+                  style: GoogleFonts.quicksand(
+                      color: const Color(0xffffa62b),
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.normal,
+                      fontSize: height * 0.04),
+                  textAlign: TextAlign.left),
             ),
-        ],
-      )
-    )
-    );
+          ],
+        )));
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:learnnfun/BusinessModelCanvas/listScreen.dart';
 import 'package:learnnfun/DesignThinking/Persona.dart';
 import 'package:learnnfun/DesignThinking/overallPages/levels.dart';
 import 'package:learnnfun/gameSelection.dart';
+import 'package:learnnfun/menupage.dart';
 import 'package:learnnfun/registerInfo.dart';
 import 'package:overlay_support/overlay_support.dart';
 
@@ -27,72 +28,74 @@ class MyApp extends StatelessWidget {
     final Future<FirebaseApp> _initialization = Firebase.initializeApp();
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      // ignore: missing_return
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return Directionality(
-              textDirection: TextDirection.ltr,
-              child: new Container(child: Text("Firebase went wrong")));
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (kDebugMode) {
-            FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
-          }
-          FlutterError.onError =
-              FirebaseCrashlytics.instance.recordFlutterError;
-          BaseAuth auth = new Auth();
-          l1Score.task = {};
-          l2Score.task = {};
-          if (auth.getCurrentUser() != null) {
-
-            if (auth.getCurrentUser().displayName == null ||
-                auth.getCurrentUser().displayName == "null") {
-              return OverlaySupport(
-                  child: MaterialApp(
-                      debugShowCheckedModeBanner: false,
-                      title: 'Learn N Fun',
-                      theme: ThemeData(
-                        fontFamily: 'Quicksand',
-                        primaryColor: const Color(0xff16697a), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color(0xffffa62b)),
-                      ),
-                      home: RegistrationInfo()));
-            } else {
-              return OverlaySupport(
-                  child: MaterialApp(
-                      debugShowCheckedModeBanner: false,
-                      title: 'Learn N Fun',
-                      theme: ThemeData(
-                        fontFamily: 'Quicksand',
-                        primaryColor: const Color(0xff16697a), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color(0xffffa62b)),
-                      ),
-                      home:
-                      GameSelection())); //TODO: Change this to whatever you want while testing, but change to GameSelection() before pushing
-
-
-              //TaskCompleted(rewards: 10, task: true, trophies: 10)));
-            }
-          } else {
-
-            return OverlaySupport(
-                child: MaterialApp(
-                    debugShowCheckedModeBanner: false,
-                    title: 'Learn N Fun',
-                    theme: ThemeData(
-                      fontFamily: 'Quicksand',
-                      primaryColor: const Color(0xff16697a), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color(0xffffa62b)),
-                    ),
-                    home: Login()));
-          }
-        }
-        // Otherwise, show something whilst waiting for initialization to complete
-        return CircularProgressIndicator(); //TODO : Change to Splash Screen
-      },
+    return MaterialApp(
+      home: MenuPage(),
     );
+    // return FutureBuilder(
+    //   // Initialize FlutterFire:
+    //   future: _initialization,
+    //   // ignore: missing_return
+    //   builder: (context, snapshot) {
+    //     // Check for errors
+    //     if (snapshot.hasError) {
+    //       return Directionality(
+    //           textDirection: TextDirection.ltr,
+    //           child: new Container(child: Text("Firebase went wrong")));
+    //     }
+
+    //     // Once complete, show your application
+    //     if (snapshot.connectionState == ConnectionState.done) {
+    //       if (kDebugMode) {
+    //         FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+    //       }
+    //       FlutterError.onError =
+    //           FirebaseCrashlytics.instance.recordFlutterError;
+    //       BaseAuth auth = new Auth();
+    //       l1Score.task = {};
+    //       l2Score.task = {};
+    //       if (auth.getCurrentUser() != null) {
+
+    //         if (auth.getCurrentUser().displayName == null ||
+    //             auth.getCurrentUser().displayName == "null") {
+    //           return OverlaySupport(
+    //               child: MaterialApp(
+    //                   debugShowCheckedModeBanner: false,
+    //                   title: 'Learn N Fun',
+    //                   theme: ThemeData(
+    //                     fontFamily: 'Quicksand',
+    //                     primaryColor: const Color(0xff16697a), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color(0xffffa62b)),
+    //                   ),
+    //                   home: RegistrationInfo()));
+    //         } else {
+    //           return OverlaySupport(
+    //               child: MaterialApp(
+    //                   debugShowCheckedModeBanner: false,
+    //                   title: 'Learn N Fun',
+    //                   theme: ThemeData(
+    //                     fontFamily: 'Quicksand',
+    //                     primaryColor: const Color(0xff16697a), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color(0xffffa62b)),
+    //                   ),
+    //                   home:
+    //                   GameSelection())); //TODO: Change this to whatever you want while testing, but change to GameSelection() before pushing
+
+    //           //TaskCompleted(rewards: 10, task: true, trophies: 10)));
+    //         }
+    //       } else {
+
+    //         return OverlaySupport(
+    //             child: MaterialApp(
+    //                 debugShowCheckedModeBanner: false,
+    //                 title: 'Learn N Fun',
+    //                 theme: ThemeData(
+    //                   fontFamily: 'Quicksand',
+    //                   primaryColor: const Color(0xff16697a), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color(0xffffa62b)),
+    //                 ),
+    //                 home: Login()));
+    //       }
+    //     }
+    //     // Otherwise, show something whilst waiting for initialization to complete
+    //     return CircularProgressIndicator(); //TODO : Change to Splash Screen
+    //   },
+    // );
   }
 }
